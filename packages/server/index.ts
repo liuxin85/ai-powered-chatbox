cee664f82e59cc3f87afd7554a1f0b97b14d4094
liuxin85 <liuxin85@gmail.com>
2025-10-26 15:25:06 +0800

    Set up prettier

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,17 +1,17 @@
-import express from "express";
-import type { Request, Response } from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hell world");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hell world');
 });
-app.get("/api/hello", (req: Request, res: Response) => {
-  res.json({ message: "hello world" });
+app.get('/api/hello', (req: Request, res: Response) => {
+  res.json({ message: 'hello world' });
 });
 
 app.listen(port, () => {
