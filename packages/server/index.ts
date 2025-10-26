8e3b480b49ee469e24e12f5cdc315558e955bc50
liuxin85 <liuxin85@gmail.com>
2025-10-26 21:47:10 +0800

    feat: converstaion repository

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
+import z from 'zod';
+import { conversationRepository } from './repositories/conversation.repository';
 
 dotenv.config();
 
@@ -20,22 +22,39 @@ app.get('/api/hello', (req: Request, res: Response) => {
   res.json({ message: 'hello world' });
 });
 
-const conversations = new Map<string, string>();
+const chatSchema = z.object({
+  prompt: z
+    .string()
+    .trim()
+    .min(1, 'Prompt is required')
+    .max(1000, 'Prompt is too long (max 1000 characters'),
+  conversationId: z.uuid(),
+});
 
 app.post('/api/chat', async (req: Request, res: Response) => {
-  const { prompt, conversationId } = req.body;
+  const parseResult = chatSchema.safeParse(req.body);
+  if (!parseResult.success) {
+    res.status(400).json(parseResult.error.format());
+    return;
+  }
 
-  const response = await client.responses.create({
-    model: 'gpt-4o-mini',
-    input: prompt,
-    temperature: 0.2,
-    max_output_tokens: 100,
-    previous_response_id: conversations.get(conversationId),
-  });
+  try {
+    const { prompt, conversationId } = req.body;
+    const response = await client.responses.create({
+      model: 'gpt-4o-mini!',
+      input: prompt,
+      temperature: 0.2,
+      max_output_tokens: 100,
+      previous_response_id:
+        conversationRepository.getLastResponseId(conversationId),
+    });
 
-  conversations.set(conversationId, response.id);
+    conversationRepository.setLastResponseId(conversationId, response.id);
 
-  res.json({ message: response.output_text });
+    res.json({ message: response.output_text });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to generate a response' });
+  }
 });
 
 app.listen(port, () => {
